3bf373178e1c48e2164ca966f11d2680cca343f6
cjamadagni <chirag.jamadagni@gmail.com>
2016-03-12 15:26:33 +0530

    2D to 3D conversion

--- examples/test.jl
+++ examples/test.jl
@@ -6,17 +6,36 @@ using ThreeJS
 
 main(window) = begin
     num = Input(10)
+    toggle = Input(false)
+    
     push!(window.assets,("ThreeJS","threejs"))
     push!(window.assets,"widgets")
         vbox(
-        "WheelGraph Example",
+        h1("WheelGraph Example"),
         vskip(2em),
         vbox(
             "Number of Nodes",
             slider(10:100) >>> num
         ),
-        lift(num) do n
-            drawwheel2D(n)
+        vskip(2em),
+        vbox(
+            "2D/3D",
+            hskip(2em),
+            togglebutton() >>> toggle 
+        ),
+        vskip(2em),
+
+        lift(toggle) do t
+            if t        
+                lift(num) do n
+                    drawwheel3D(n)
+                end
+            
+            else
+                lift(num) do n
+                    drawwheel2D(n)
+                end
+            end
         end
         ) |> pad(2em)
 end
