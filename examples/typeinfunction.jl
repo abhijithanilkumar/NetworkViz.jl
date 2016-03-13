05799a65a706c1fa2d2dedd02d818f0faf7f8381
Abhijith Anilkumar <abhijithanilkumar@live.com>
2016-03-14 04:33:05 +0530

    Example - Type in functions

---
+++ examples/typeinfunction.jl
@@ -0,0 +1,64 @@
+using ThreeJS
+using Colors: colormap
+using LightGraphs
+using NetworkViz
+
+# Run in Escher to get updatable surf and mesh plots
+
+g = Graph(10)
+
+function surf(f::Function)
+ ThreeJS.outerdiv() <<
+            (ThreeJS.initscene() <<
+                [
+                    ThreeJS.mesh(0.0, 0.0, 0.0) <<
+                    [
+                        ThreeJS.parametric(100,100,-10:10, -10:10,f),
+                        ThreeJS.material(Dict(:kind=>"lambert",:color=>"white",
+                        :colorkind=>"vertex"));
+                    ],
+                    ThreeJS.ambientlight(),
+                    ThreeJS.camera(30.0, 30.0, 30.0)
+                ]
+            )
+end
+
+function mesh(f::Function)
+ ThreeJS.outerdiv() <<
+            (ThreeJS.initscene() <<
+                [
+                    ThreeJS.meshlines(50, 50, -10:10, -10:10, f),
+                    ThreeJS.ambientlight(),
+                    ThreeJS.camera(30.0, 30.0, 30.0);
+                ]
+            )
+end
+main(window) =  begin
+    push!(window.assets,("ThreeJS","threejs"))
+    push!(window.assets,"widgets")
+    push!(window.assets, "codemirror")
+    push!(window.assets, "layout2")
+    default = "(x,y) -> sin(x) * cos(y)"
+    inp = Signal(Dict{Any, Any}(:name=>default))
+    s = sampler() # A thing that lets you watch widgets/behaviors upon updates to other behaviors
+    editor = watch!(s, :code, codemirror(default))
+    code_cell = trigger!(s, :submit, keypress("ctrl+enter shift+enter", editor))
+    map(inp) do f
+        fn = get(f,:code,default)
+        eval(parse(fn))
+        @show g
+        plugsampler(s,
+        vbox(
+                md"""Enter an anonymous function with 2 variables.
+                    `ctrl+enter` or `shift+enter` to redraw the plot.
+                    Use the mouse the drag, zoom and pan.
+                    The function is plotted with x and y between -10 and 10 and
+                    with 100 steps and 50 steps in both axes for the surf and the
+                    mesh respectively. Try resizing the browser if you cant see a codebox""",
+                code_cell,
+                vskip(2em),
+            drawGraph(g,1)
+            ) |> pad(2em)
+   ) >>> inp
+ end
+end
