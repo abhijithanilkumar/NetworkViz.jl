c2995d83194401e127f1a10aed32d7ce8d937491
Abhijith Anilkumar <abhijithanilkumar@live.com>
2016-03-15 00:12:05 +0530

    Walk Example

---
+++ examples/edgeanim.jl
@@ -0,0 +1,14 @@
+using ThreeJS
+using NetworkViz
+using LightGraphs
+
+main(window) =  begin
+    g = Graph(10)
+    push!(window.assets,("ThreeJS","threejs"))
+    fps1 = fps(1)
+    frames = foldp(+, 1, map((x)->1, fps1))
+    running = map(x->x<9, frames)
+    map(fpswhen(running, 1)) do _
+        addEdge(g,frames.value,frames.value+1,1)
+    end
+end
