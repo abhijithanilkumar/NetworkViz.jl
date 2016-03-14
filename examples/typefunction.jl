e5c673bb534b71d52e6a3f4560c4100cbd394249
Abhijith Anilkumar <abhijithanilkumar@live.com>
2016-03-15 05:22:21 +0530

    Fix CI Issues

--- examples/typefunction.jl
+++ examples/typefunction.jl
@@ -1,39 +1,11 @@
-# Example to demonstrate the use of a codemirror. 
+# Example to demonstrate the use of a codemirror.
 
 using ThreeJS
-using Colors: colormap
 using LightGraphs
 using NetworkViz
 
-
 g = Graph(10)
 
-function surf(f::Function)
- ThreeJS.outerdiv() <<
-            (ThreeJS.initscene() <<
-                [
-                    ThreeJS.mesh(0.0, 0.0, 0.0) <<
-                    [
-                        ThreeJS.parametric(100,100,-10:10, -10:10,f),
-                        ThreeJS.material(Dict(:kind=>"lambert",:color=>"white",
-                        :colorkind=>"vertex"));
-                    ],
-                    ThreeJS.ambientlight(),
-                    ThreeJS.camera(30.0, 30.0, 30.0)
-                ]
-            )
-end
-
-function mesh(f::Function)
- ThreeJS.outerdiv() <<
-            (ThreeJS.initscene() <<
-                [
-                    ThreeJS.meshlines(50, 50, -10:10, -10:10, f),
-                    ThreeJS.ambientlight(),
-                    ThreeJS.camera(30.0, 30.0, 30.0);
-                ]
-            )
-end
 main(window) =  begin
     push!(window.assets,("ThreeJS","threejs"))
     push!(window.assets,"widgets")
